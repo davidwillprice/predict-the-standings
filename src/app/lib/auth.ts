3303ee71f398825859488005f8e3a32b59313cc5
David Price <davidwillprice@gmail.com>
2024-01-10 17:39:15 +0000

    Connect NextAuth to Postgres
    - Install pg and pg-adapter

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,7 +1,41 @@
 import GoogleProvider from "next-auth/providers/google";
 import RedditProvider from "next-auth/providers/reddit";
+import { JWT } from "next-auth/jwt";
+import { Session, User } from "next-auth";
+import PostgresAdapter from "@auth/pg-adapter";
+import type { Adapter } from "next-auth/adapters";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST as string,
+  user: process.env.POSTGRES_USER as string,
+  password: process.env.POSTGRES_PASSWORD as string,
+  database: process.env.POSTGRES_DATABASE as string,
+  max: 20,
+  ssl: true,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
 
 export const authOptions = {
+  debug: true,
+  adapter: PostgresAdapter(pool) as Adapter,
+  callbacks: {
+    async session({
+      session,
+      token,
+      user,
+    }: {
+      session: Session;
+      token: JWT;
+      user: User;
+    }) {
+      // Send properties to the client, like an access_token and user id from a provider.
+      //console.log(session, token, user);
+
+      return session;
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
