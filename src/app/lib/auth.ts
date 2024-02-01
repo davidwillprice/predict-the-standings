bce878ffaabbf614c426a9244a9c1b3f8301954e
David Price <davidwillprice@gmail.com>
2024-02-01 21:08:46 +0000

    Move DB connection to its own file

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -4,21 +4,10 @@ import { JWT } from "next-auth/jwt";
 import { Session, User } from "next-auth";
 import PostgresAdapter from "@auth/pg-adapter";
 import type { Adapter } from "next-auth/adapters";
-import { Pool } from "pg";
-
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST as string,
-  user: process.env.POSTGRES_USER as string,
-  password: process.env.POSTGRES_PASSWORD as string,
-  database: process.env.POSTGRES_DATABASE as string,
-  max: 20,
-  ssl: true,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+import { pool } from "@lib/db";
 
 export const authOptions = {
-  debug: true,
+  //debug: true,
   adapter: PostgresAdapter(pool) as Adapter,
   callbacks: {
     async session({
@@ -31,8 +20,6 @@ export const authOptions = {
       user: User;
     }) {
       // Send properties to the client, like an access_token and user id from a provider.
-      //console.log(session, token, user);
-
       return session;
     },
   },
---
+++ src/app/lib/db.ts
@@ -0,0 +1,19 @@
+import { Pool, QueryResultRow } from "pg";
+
+export const pool = new Pool({
+  host: process.env.POSTGRES_HOST as string,
+  user: process.env.POSTGRES_USER as string,
+  password: process.env.POSTGRES_PASSWORD as string,
+  database: process.env.POSTGRES_DATABASE as string,
+  max: 20,
+  ssl: true,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+export const query = <Result extends QueryResultRow>(
+  text: string,
+  params: any[] = []
+) => {
+  return pool.query<Result>(text, params);
+};
