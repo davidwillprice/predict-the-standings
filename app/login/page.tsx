96155850b479dd384456b5e5491058edb821e8b3
David Price <davidwillprice@gmail.com>
2024-02-03 16:10:52 +0000

    Add login todos

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,8 @@ import { LoginForm } from "@components/login/login-form";
 import { PanelHeading } from "@components/panels/panel-heading";
 
 const Page = () => {
+  /**@todo Redirect from page if signed in*/
+  /**@todo Fix Reddit issue on PTS.com `An error occurred in the Server Components render. The specific message is omitted in production builds to avoid leaking sensitive details. A digest property is included on this error instance which may provide additional details about the nature of the error. Digest: 725504142`*/
   return (
     <>
       <PanelHeading>
