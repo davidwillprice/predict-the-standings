587726611bf8347cde864e5d8920385d92f0045c
David Price <davidwillprice@gmail.com>
2024-02-04 15:13:41 +0000

    Added rough terms of service page

---
+++ app/terms-of-service/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Terms of Service | Predict The Standings",
+};
+
+export default function Page() {
+  return <p>Example</p>;
+}
