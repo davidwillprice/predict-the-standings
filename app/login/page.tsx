697a4e77dc7d378660ea813f7aff77cad7a35abe
David Price <davidwillprice@gmail.com>
2024-02-04 08:59:25 +0000

    Redirect away from login page if the user is logged in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,17 @@
+import { getServerSession } from "next-auth/next";
+import { redirect } from "next/navigation";
+
+import { authOptions } from "@lib/auth";
+
 import { Panel } from "@components/panels/panel";
 import { LoginForm } from "@components/login/login-form";
 import { PanelHeading } from "@components/panels/panel-heading";
 
-const Page = () => {
-  /**@todo Redirect from page if signed in*/
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+  if (session !== null) {
+    return redirect("/profile");
+  }
   /**@todo Fix Reddit issue on PTS.com `An error occurred in the Server Components render. The specific message is omitted in production builds to avoid leaking sensitive details. A digest property is included on this error instance which may provide additional details about the nature of the error. Digest: 725504142`*/
   return (
     <>
