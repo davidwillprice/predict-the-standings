9a918364421e759f90be79c3590b646c1c128924
David Price <davidwillprice@gmail.com>
2024-02-13 20:33:42 +0000

    Change how button width is decided

--- src/ui/components/login/login-form.tsx
+++ src/ui/components/login/login-form.tsx
@@ -15,44 +15,46 @@ export const LoginForm = () => (
        * Apple developer program costs $99py so can't add Apple provider*/}
       {/**@todo Add Linkedin login */}
       {/**@todo Fix Patreon & Spotify logins */}
-      <Button
-        onClick={async () => {
-          await signIn("github", { redirect: false });
-        }}>
-        Sign in via Github
-      </Button>
-      <Button
-        onClick={async () => {
-          await signIn("google", { redirect: false });
-        }}>
-        Sign in via Google
-      </Button>
-      {/** 
+      <div className={styles.btn_con}>
+        <Button
+          onClick={async () => {
+            await signIn("github", { redirect: false });
+          }}>
+          Sign in via Github
+        </Button>
+        <Button
+          onClick={async () => {
+            await signIn("google", { redirect: false });
+          }}>
+          Sign in via Google
+        </Button>
+        {/** 
       <Button
         onClick={async () => {
           await signIn("patreon", { redirect: false });
         }}>
         Sign in via Patreon
       </Button>*/}
-      <Button
-        onClick={async () => {
-          await signIn("reddit", { redirect: false });
-        }}>
-        Sign in via Reddit
-      </Button>
-      {/**
+        <Button
+          onClick={async () => {
+            await signIn("reddit", { redirect: false });
+          }}>
+          Sign in via Reddit
+        </Button>
+        {/**
        <Button
         onClick={async () => {
           await signIn("spotify", { redirect: false });
         }}>
         Sign in via Spotify
       </Button>*/}
-      <Button
-        onClick={async () => {
-          await signIn("twitter", { redirect: false });
-        }}>
-        Sign in via Twitter
-      </Button>
+        <Button
+          onClick={async () => {
+            await signIn("twitter", { redirect: false });
+          }}>
+          Sign in via Twitter
+        </Button>
+      </div>
     </div>
   </>
 );
