af9a4e5cd51612e774c2b38e0eac6379e60eee24
David Price <davidwillprice@gmail.com>
2024-02-15 22:53:59 +0000

    Flesh out homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,50 @@
+import Link from "next/link";
+
+import { PanelHeading } from "@components/panels/panel-heading";
 import { Panel } from "@components/panels/panel";
+import { Button } from "@components/button/button";
+import Icon from "@svgs/icons/sq-icon";
+
+import styles from "@components/button/button-containers.module.scss";
 
 export default function Home() {
   return (
     <>
+      <PanelHeading>
+        <h1>Predict The Standings</h1>
+      </PanelHeading>
       <Panel>
         {/**@todo URGENT Add text including F1 disclaimer */}
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras volutpat
-          dignissim imperdiet. Suspendisse lobortis sollicitudin libero id
-          eleifend. Quisque id justo tincidunt, efficitur tortor id, malesuada
-          dui. Nulla gravida, ipsum at mollis pharetra, lorem augue tempor eros,
-          in elementum justo augue sed lectus. Suspendisse potenti. Proin vitae
-          metus vel augue blandit porttitor non in velit. Proin at odio id purus
-          mattis bibendum quis non neque. Morbi in finibus purus, ornare
-          dignissim nunc. Aenean id ligula velit. Suspendisse vitae rutrum
-          sapien. Mauris laoreet dictum nulla, et dignissim nisi molestie ac.
+          Compete to against people around the world to Predict The Standings
+          for various sports.
         </p>
-      </Panel>
-      <Panel>
-        <p>
-          Suspendisse vitae rutrum sapien. Mauris laoreet dictum nulla, et
-          dignissim nisi molestie ac. Proin malesuada tristique justo, in
-          pellentesque erat vestibulum quis. Phasellus eleifend varius
-          malesuada. Vivamus pharetra lobortis semper. Sed vel ipsum at sem
-          interdum ullamcorper non id turpis. Nunc augue odio, pretium malesuada
-          faucibus quis, pulvinar in neque. Sed augue purus, tincidunt ut metus
-          lacinia, pulvinar imperdiet dui. Donec a dignissim diam, ac dapibus
-          erat. Proin sit amet enim quis nibh convallis sollicitudin. In
-          euismod, sapien at feugiat eleifend, justo eros laoreet arcu, et
-          lobortis libero enim vitae orci. Pellentesque sed sem in velit
-          ultricies rutrum. Nam a lacus ac quam facilisis accumsan. Phasellus
-          semper varius tortor, eget dignissim ante posuere vitae.
-        </p>
-      </Panel>
-      <Panel>
-        <p>
-          Suspendisse ligula mi, vehicula venenatis est id, maximus luctus
-          ligula. Morbi quis eros non risus lacinia scelerisque. Vestibulum
-          volutpat tempus consectetur. Integer elementum ultrices tincidunt.
-          Quisque scelerisque ipsum suscipit, faucibus turpis et, scelerisque
-          dui. Nunc euismod dolor vel mauris scelerisque luctus. In fermentum
-          auctor risus, eget tempor nisi viverra nec. Phasellus sit amet rutrum
-          magna, sed efficitur odio. Quisque ac sem eu tortor faucibus ornare eu
-          dapibus nibh. Sed in dignissim orci, eget ultrices massa. Donec urna
-          erat, vehicula suscipit sem vel, tempus pellentesque massa. Nulla sed
-          odio diam. Ut nec quam eget ante viverra sodales vel at ante. Quisque
-          mattis arcu a enim iaculis, aliquam volutpat augue fermentum. Aliquam
-          iaculis porttitor lacus vitae vulputate. Integer eu sem non erat
-          eleifend dapibus. Integer rutrum felis egestas velit tempus ultrices.
-          Integer id pulvinar augue. Nunc non urna est. Morbi nulla nunc,
-          vulputate eget feugiat ac, elementum eget felis. Donec viverra felis
-          eu lorem lacinia, in varius metus suscipit. Nulla sed diam odio.
-          Pellentesque massa sem, convallis vel mollis et, efficitur vel metus.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris
-          nibh odio, feugiat id convallis nec, fermentum non velit. Donec et
-          metus eu odio ornare sodales. Praesent non vulputate justo. Sed eget
-          urna arcu. Vestibulum ligula nisi, consequat a finibus quis,
-          sollicitudin sit amet nisi. Sed at orci hendrerit, viverra justo et,
-          egestas tellus. Curabitur ut dui id mauris tempor tristique. Maecenas
-          magna dolor, mattis a aliquet sit amet, venenatis quis mi. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Quisque tristique diam
-          sit amet diam pretium tincidunt. Nam in eros ex. Nulla facilisi.
-        </p>
+        <ul>
+          <li>
+            The leaderboard will be updated after each round so you can see how
+            your predictions are performing throughout the season.
+          </li>
+          <li>
+            View stats and trivia on how controversial your each of predictions
+            are, and how accurately players have predicted each
+            driver/team&apos;s standing.
+          </li>
+        </ul>
+        <hr />
+        <div className={styles.double}>
+          <Link href="/formula-1/">
+            <Button>
+              <Icon strokeWidth={2} type="f1" />
+              Formula 1
+            </Button>
+          </Link>
+          <Link href="/premier-league/">
+            <Button>
+              <Icon strokeWidth={2} type="premierLeague" />
+              Premier League
+            </Button>
+          </Link>
+        </div>
       </Panel>
     </>
   );
