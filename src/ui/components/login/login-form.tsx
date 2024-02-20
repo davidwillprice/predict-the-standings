8b09e9e35c360dc2bd5c06d07fc4073dc7f9bf87
David Price <davidwillprice@gmail.com>
2024-02-20 20:47:45 +0000

    Login improvements
    - Refactor
    - Add loading spinner

--- src/ui/components/login/login-form.tsx
+++ src/ui/components/login/login-form.tsx
@@ -1,84 +1,91 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 import { Button } from "@components/button/button";
+import { LoadingSpinner } from "@components/loading-spinner/loading-spinner";
 
 import styles from "@components/login/login.module.scss";
 import Icon from "@ui/svgs/icons/sq-icon";
 
-export const LoginForm = () => (
-  <>
-    <div className={styles.login}>
-      {/**@todo Add icons for each login */}
-      {/**@todo Add loading spinner */}
-      {/**Instagram/FB provider seems to require a verified FB business account
-       * Apple developer program costs $99py so can't add Apple provider*/}
-      {/**@todo Add Linkedin (requires LinkedIn Page) login */}
-      {/**@todo Fix Patreon & Spotify logins */}
-      <div className={styles.btn_con}>
-        <Button
-          className={styles.discord}
-          onClick={async () => {
-            await signIn("discord", { redirect: false });
-          }}>
-          <Icon strokeWidth={2} type="discord" />
-          Sign in via Discord
-        </Button>
-        {/** 
-        <Button
-          onClick={async () => {
-            await signIn("facebook", { redirect: false });
-          }}>
-          Sign in via Facebook
-        </Button>*/}
-        <Button
-          className={styles.github}
-          onClick={async () => {
-            await signIn("github", { redirect: false });
-          }}>
-          <Icon strokeWidth={2} type="github" />
-          Sign in via Github
-        </Button>
-        <Button
-          className={styles.google}
-          onClick={async () => {
-            await signIn("google", { redirect: false });
-          }}>
-          <Icon strokeWidth={2} type="google" />
-          Sign in via Google
-        </Button>
-        {/** 
-      <Button
-        onClick={async () => {
-          await signIn("patreon", { redirect: false });
-        }}>
-        Sign in via Patreon
-      </Button>*/}
-        <Button
-          className={styles.reddit}
-          onClick={async () => {
-            await signIn("reddit", { redirect: false });
-          }}>
-          <Icon strokeWidth={2} type="reddit" />
-          Sign in via Reddit
-        </Button>
-        {/**
-       <Button
-        onClick={async () => {
-          await signIn("spotify", { redirect: false });
-        }}>
-        Sign in via Spotify
-      </Button>*/}
-        <Button
-          className={styles.twitter}
-          onClick={async () => {
-            await signIn("twitter", { redirect: false });
-          }}>
-          <Icon strokeWidth={2} type="twitter" />
-          Sign in via Twitter
-        </Button>
+export const LoginForm = () => {
+  const [submitting, isSubmitting] = useState(false);
+
+  const signInHandler = async (e: React.MouseEvent<HTMLElement>) => {
+    isSubmitting(true);
+    await signIn(e.currentTarget.id, { redirect: false });
+  };
+
+  return (
+    <>
+      <div className={`${styles.login} ${submitting ? styles.loading : ""}`}>
+        {/**Instagram/FB provider seems to require a verified FB business account
+         * Apple developer program costs $99py so can't add Apple provider*/}
+        {/**@todo Add Linkedin (requires LinkedIn Page) login */}
+        {/**@todo Fix Patreon & Spotify logins */}
+        {submitting ? (
+          <LoadingSpinner />
+        ) : (
+          <div className={styles.btn_con}>
+            <Button
+              id="discord"
+              className={styles.discord}
+              onClick={signInHandler}>
+              <Icon strokeWidth={2} type="discord" />
+              Sign in via Discord
+            </Button>
+            {/** 
+            <Button
+              onClick={async () => {
+                await signIn("facebook", { redirect: false });
+              }}>
+              Sign in via Facebook
+            </Button>*/}
+            <Button
+              id="github"
+              className={styles.github}
+              onClick={signInHandler}>
+              <Icon strokeWidth={2} type="github" />
+              Sign in via Github
+            </Button>
+            <Button
+              id="google"
+              className={styles.google}
+              onClick={signInHandler}>
+              <Icon strokeWidth={2} type="google" />
+              Sign in via Google
+            </Button>
+            {/** 
+            <Button
+              onClick={async () => {
+                await signIn("patreon", { redirect: false });
+              }}>
+              Sign in via Patreon
+            </Button>*/}
+            <Button
+              id="reddit"
+              className={styles.reddit}
+              onClick={signInHandler}>
+              <Icon strokeWidth={2} type="reddit" />
+              Sign in via Reddit
+            </Button>
+            {/* <Button
+              onClick={async () => {
+                await signIn("spotify", { redirect: false });
+              }}>
+              Sign in via Spotify
+            </Button> */}
+            <Button
+              id="twitter"
+              className={styles.twitter}
+              onClick={signInHandler}>
+              <Icon strokeWidth={2} type="twitter" />
+              Sign in via Twitter
+            </Button>
+          </div>
+        )}
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
