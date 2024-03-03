aa4c5bb522386d9f6b160f0b080d31189d1e1212
David Price <davidwillprice@gmail.com>
2024-03-03 15:45:32 +0000

    Re-add Sport string type

--- src/custom-types/misc.tsx
+++ src/custom-types/misc.tsx
@@ -2,3 +2,5 @@ export interface PageProps {
   searchParams: { [key: string]: string | string[] | undefined };
   params: { [key: string]: string };
 }
+
+export type Sport = "f1" | "premier_league";
