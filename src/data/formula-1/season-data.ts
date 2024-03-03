ca2b3442f414469aa817b7e68e7b6fdba74e5887
David Price <davidwillprice@gmail.com>
2024-03-03 15:27:04 +0000

    Add 2023 season data

--- src/data/formula-1/season-data.ts
+++ src/data/formula-1/season-data.ts
@@ -1,4 +1,5 @@
 import { seasonData2024 } from "./2024";
+import { seasonData2023 } from "./2023";
 import type { Entrant, Round } from "@custom-types/game-types";
 
 interface AllSeasonData {
@@ -12,5 +13,6 @@ interface AllSeasonData {
 }
 
 export const allSeasonData: AllSeasonData = {
+  "2023": seasonData2023,
   "2024": seasonData2024,
 };
